c2c9e62644e8d51aa0e42da919bb800291b5c0a6
s00149173 <s00149173@mail.itsligo.ie>
2014-02-18 21:31:49 +0000

    Added popup windows,opened by a button on legs list, to show the guests list (not showing yet)

---
+++ TourAgency/Controllers/LegController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TourAgency.Models;
+
+namespace TourAgency.Controllers
+{
+    public class LegController : Controller
+    {
+        private TourAgencyEntities db = new TourAgencyEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GuestList(int id)
+        {
+            Leg leg = db.Legs.Where(l => l.LegId == id).Cast<Leg>().First();
+            return View("_Guests", leg);
+        }
+
+    }
+}
